f0f3fc7f3f3b4d9e8e2a4e3b986538f31eda1206
Akhil Ravindran <akhilravindran3@gmail.com>
2020-08-13 18:40:24 +0530

    built basic structure of the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
 // menu logic
 
-$('#menu-open').click(function () {
-    $('.navigation').slideDown();
-    $(this).hide();
-    $('#menu-close').show();
+$("#menu-open").click(function () {
+  $(".navigation").slideDown();
+  $(this).hide();
+  $("#menu-close").show();
 });
 
 $("#menu-close").click(function () {
-    $(".navigation").slideUp();
-    $(this).hide();
-    $('#menu-open').show();
+  $(".navigation").slideUp();
+  $(this).hide();
+  $("#menu-open").show();
+});
+
+// testimonial carousel logic
+$(document).ready(function () {
+  let owl = $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+  });
+
+    $(".slide-previous").click(() => {
+      console.log('previous')
+    owl.trigger("prev.owl.carousel");
+  });
+
+    $(".slide-next").click(() => {
+            console.log("next");
+
+    owl.trigger("next.owl.carousel");
+  });
 });
